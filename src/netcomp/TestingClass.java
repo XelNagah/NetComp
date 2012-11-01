e84b1e3de232ebd7af905427cd118311c1986f3c
XelNagah <xelnagah@gmail.com>
2012-10-31 21:16:24 -0300

    Limpieza de archivos sin utilizar y métodos basura
    Se borraron archivos sin utilizar, que provienen de viejas pruebas, y código de prueba de la clase TestingClass

--- src/netcomp/TestingClass.java
+++ src/netcomp/TestingClass.java
@@ -10,20 +10,8 @@ package netcomp;
  */
 
 public class TestingClass {
-    public static String miIp;
     
         public static void main(String[] args) {
             
-            
-            String stringXML = GenTools.XMLGenerator("puto", "Matias Fritz");
-            System.out.println(stringXML);
-            String xmlParseado = GenTools.XMLParser("puto", stringXML);
-            System.out.println(xmlParseado);
-            String otroStringXML = GenTools.XMLAppend("otroputo", "El Orshi", stringXML);
-            System.out.println(otroStringXML);
-            String xmlWrapper = GenTools.XMLWrapper("msg", otroStringXML);
-            System.out.println(xmlWrapper);
-            System.out.println("Parseo algo que NO existe:");
-            System.out.println(GenTools.XMLParser("bbebekbej",otroStringXML));
         }
 }
